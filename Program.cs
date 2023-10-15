53cbcdf4695feb4fd4851dbf8a639f6ec890dc64
LeandroMPio <leandropio@gmail.com>
2023-10-15 17:08:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoEscola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
